68f444de1888a02dc57bfdb07b352a63517b0ab4
temnoed <dc9257351633@gmail.com>
2016-12-18 23:33:42 +0300

    Урок1

---
+++ src/ru/geekbrains/firstproject/MainClass.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.firstproject;
+
+/**
+ * Created by Musimusimus on 18.12.2016.
+ */
+public class MainClass {
+
+    public static void main (String[] args) {
+        System.out.println("Hellow, Programmer!");
+
+        final byte a = 1;
+        short b = 2000;
+        int c = 300000;
+        long d = 400000L;
+        float e = 5.0f;
+        double f = 6.5346563;
+        boolean g = false;
+        char h = 'x';
+    }
+}
