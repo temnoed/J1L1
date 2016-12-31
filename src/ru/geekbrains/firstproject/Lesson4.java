fd77c263e935270651527f29dec2a9ce254360ed
temnoed <dc9257351633@gmail.com>
2017-01-01 02:21:35 +0300

    Крестики нолики.

--- src/ru/geekbrains/firstproject/Lesson4.java
+++ src/ru/geekbrains/firstproject/Lesson4.java
@@ -10,6 +10,12 @@ public class Lesson4 {
 	public static final char DOT_X ='X';
 	public static final char DOT_0 ='0';
 
+	public static void main(String[] args) {
+		initMap();
+		printMap();
+	}
+
+
 	public static void initMap() {
 		map = new char[SIZE][SIZE];
 		for (int i =0; i < SIZE; i++) {
@@ -18,5 +24,21 @@ public class Lesson4 {
 			}
 		}
 	}
+
+	public static void printMap() {
+		for (int i = 0; i <= SIZE; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		for (int i = 0; i < SIZE; i++) {
+			System.out.print((i + 1) + " ");
+			for (int j = 0; j < SIZE; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
 
 }
